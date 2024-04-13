806cd1728269c146d6a408ba90e7105e51630b2f
kbpatel <kbpatel3@catamount.wcu.edu>
2024-04-13 15:58:48 -0400

    Placeholder skip button

--- frontend/my-app/src/components/VideoFeedContainer.js
+++ frontend/my-app/src/components/VideoFeedContainer.js
@@ -7,10 +7,14 @@ const VideoFeedContainer = () => {
             <div className="aspect-w-16 aspect-h-9 w-full max-w-[65%]"> {/* Adjust aspect ratio and width */}
                 <VideoFeed />
             </div>
+	    <div className="w-6"></div>
             <div className="aspect-w-16 aspect-h-9 w-full max-w-[65%]"> {/* Adjust aspect ratio and width */}
                 <VideoFeed />
             </div>
         </div>
+	<div>
+	    <button>Skip</button>
+	</div>
     );
 };
 
